ee3681f4101a9e4ab76a9c9f0ed4e95b591805dc
hedrall <masaharu73@gmail.com>
2024-01-17 08:49:10 +0900

    fix

--- src/scripts/新規記事作成.ts
+++ src/scripts/新規記事作成.ts
@@ -5,7 +5,6 @@ import meta from '../posts/meta/_meta';
 import externalSetting from './投稿ツール/externalSiteSetting/_index';
 import { input } from '@inquirer/prompts';
 import { Post } from '../posts/meta';
-import Meta = Post.Meta;
 import dayjs from 'dayjs';
 
 // rootPath
@@ -16,9 +15,9 @@ const metaPath = path.resolve(postsPath, 'meta/_meta.ts');
 const externalSettingPath = path.resolve(rootPath, 'src/scripts/投稿ツール/externalSiteSetting/_index.ts');
 
 const main = async () => {
-  const slug = await input({ message: 'ファイル名 (.mdxの前まで)' });
+  const slug = await input({ message: 'slug (ファイル名の .mdx の前まで)' });
   const ファイル名 = `${slug}.mdx`;
-  const newMetaItem: Meta = {
+  const newMetaItem = {
     slug,
     title: await input({ message: 'タイトル' }),
     date: dayjs().format('YYYY-MM-DD'),
@@ -31,6 +30,7 @@ const main = async () => {
   const filePath = path.resolve(postsPath, ファイル名);
   fs.writeFileSync(filePath, '本文を書いてください。');
   console.log(`>>> ${filePath}`);
+  console.log('');
 
   console.log('2. src/posts/meta/filenames.ts に追加する');
   fs.writeFileSync(
@@ -42,6 +42,8 @@ const main = async () => {
       `export default ${JSON.stringify([ファイル名, ...filenames], null, 2)} as const;`,
     ].join('\n'),
   );
+  console.log(`>>> ${filenamePath}`);
+  console.log('');
 
   console.log('3. src/posts/meta/_meta.tsに追加する');
   fs.writeFileSync(
@@ -53,6 +55,8 @@ const main = async () => {
       `export default ${JSON.stringify({ [ファイル名]: newMetaItem, ...meta }, null, 2)} as const;`,
     ].join('\n'),
   );
+  console.log(`>>> ${metaPath}`);
+  console.log('');
 
   console.log('4. src/scripts/投稿ツール/externalSiteSetting/index.tsに追加する');
   const newExternalSettingItem = {
@@ -75,8 +79,14 @@ const main = async () => {
       `export default ${JSON.stringify({ [slug]: newExternalSettingItem, ...externalSetting }, null, 2)} as const;`,
     ].join('\n'),
   );
+  console.log(`>>> ${externalSettingPath}`);
+  console.log('');
 
   console.log('<<<<<< 完了 >>>>>>');
+  console.log(`${path.resolve(postsPath, 'meta/components.ts')} は手動で更新してください`);
+  console.log('以下を追加する');
+  const newComponent = `'${ファイル名}': dynamic(() => import('../${ファイル名}')),`;
+  console.log(newComponent);
 };
 
 (async () => {
