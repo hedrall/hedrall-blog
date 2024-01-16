175e27822d3c634ed781192a1d5e39f612fbb93e
hedrall <masaharu73@gmail.com>
2024-01-17 08:39:12 +0900

    cliの基礎を作成

---
+++ src/scripts/新規記事作成.ts
@@ -0,0 +1,84 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import filenames from '../posts/meta/_filename';
+import meta from '../posts/meta/_meta';
+import externalSetting from './投稿ツール/externalSiteSetting/_index';
+import { input } from '@inquirer/prompts';
+import { Post } from '../posts/meta';
+import Meta = Post.Meta;
+import dayjs from 'dayjs';
+
+// rootPath
+const rootPath = path.resolve(__dirname, '../../');
+const postsPath = path.resolve(rootPath, 'src/posts');
+const filenamePath = path.resolve(postsPath, 'meta/_filename.ts');
+const metaPath = path.resolve(postsPath, 'meta/_meta.ts');
+const externalSettingPath = path.resolve(rootPath, 'src/scripts/投稿ツール/externalSiteSetting/_index.ts');
+
+const main = async () => {
+  const slug = await input({ message: 'ファイル名 (.mdxの前まで)' });
+  const ファイル名 = `${slug}.mdx`;
+  const newMetaItem: Meta = {
+    slug,
+    title: await input({ message: 'タイトル' }),
+    date: dayjs().format('YYYY-MM-DD'),
+    author: 'hedrall',
+    image: '/images/typescript/logo.png',
+    tags: (await input({ message: 'タグ (カンマ区切り)' })).split(','),
+  };
+
+  console.log('1. src/postsに空のmdxファイルを作成します。');
+  const filePath = path.resolve(postsPath, ファイル名);
+  fs.writeFileSync(filePath, '本文を書いてください。');
+  console.log(`>>> ${filePath}`);
+
+  console.log('2. src/posts/meta/filenames.ts に追加する');
+  fs.writeFileSync(
+    filenamePath,
+    [
+      //
+      '/* eslint-disable */',
+      '// prettier-ignore',
+      `export default ${JSON.stringify([ファイル名, ...filenames], null, 2)} as const;`,
+    ].join('\n'),
+  );
+
+  console.log('3. src/posts/meta/_meta.tsに追加する');
+  fs.writeFileSync(
+    metaPath,
+    [
+      //
+      '/* eslint-disable */',
+      '// prettier-ignore',
+      `export default ${JSON.stringify({ [ファイル名]: newMetaItem, ...meta }, null, 2)} as const;`,
+    ].join('\n'),
+  );
+
+  console.log('4. src/scripts/投稿ツール/externalSiteSetting/index.tsに追加する');
+  const newExternalSettingItem = {
+    fileId: slug,
+    enable: true,
+    zenn: {
+      emoji: '',
+      slug,
+    },
+    qiita: {
+      id: '未投稿',
+    },
+  };
+  fs.writeFileSync(
+    externalSettingPath,
+    [
+      //
+      '/* eslint-disable */',
+      '// prettier-ignore',
+      `export default ${JSON.stringify({ [slug]: newExternalSettingItem, ...externalSetting }, null, 2)} as const;`,
+    ].join('\n'),
+  );
+
+  console.log('<<<<<< 完了 >>>>>>');
+};
+
+(async () => {
+  await main();
+})().catch(console.error);
