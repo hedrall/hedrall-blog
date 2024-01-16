153ecac3597eb4beca462a337994a4f263d92f99
hedrall <masaharu73@gmail.com>
2024-01-17 08:56:28 +0900

    fix

--- src/scripts/投稿ツール/externalSiteSetting/_index.ts
+++ src/scripts/投稿ツール/externalSiteSetting/_index.ts
@@ -1,259 +1,259 @@
 /* eslint-disable */
 // prettier-ignore
 export default {
-  '2024-01-17-not-graphql-on-sc-sys': {
-    fileId: '2024-01-17-not-graphql-on-sc-sys',
+  "2024-01-17-not-graphql-on-sc-sys": {
+    fileId: "2024-01-17-not-graphql-on-sc-sys",
     enable: true,
     zenn: {
-      emoji: '🤔',
-      slug: '2024-01-17-not-graphql-on-sc-sys',
+      emoji: "🤔",
+      slug: "2024-01-17-not-graphql-on-sc-sys",
     },
     qiita: {
-      id: 'b2abaaf44ba6dc305b52',
-      url: 'https://qiita.com/hedrall/items/b2abaaf44ba6dc305b52',
-      updatedAt: '2024-01-17T07:56:00+09:00',
+      id: "b2abaaf44ba6dc305b52",
+      url: "https://qiita.com/hedrall/items/b2abaaf44ba6dc305b52",
+      updatedAt: "2024-01-17T07:56:00+09:00",
     },
   },
-  '2023-10-24-no-more-rest': {
-    fileId: '2023-10-24-no-more-rest',
+  "2023-10-24-no-more-rest": {
+    fileId: "2023-10-24-no-more-rest",
     enable: true,
     zenn: {
-      emoji: '',
-      slug: '2023-10-24-no-more-rest',
+      emoji: "",
+      slug: "2023-10-24-no-more-rest",
     },
     qiita: {
-      id: 'a1a486993ff000eb6809',
-      url: 'https://qiita.com/hedrall/items/a1a486993ff000eb6809',
-      updatedAt: '2024-01-11T08:04:22+09:00',
+      id: "a1a486993ff000eb6809",
+      url: "https://qiita.com/hedrall/items/a1a486993ff000eb6809",
+      updatedAt: "2024-01-11T08:04:22+09:00",
     },
   },
-  '2023-08-18-ts-module-and-namespace': {
-    fileId: '2023-08-18-ts-module-and-namespace',
+  "2023-08-18-ts-module-and-namespace": {
+    fileId: "2023-08-18-ts-module-and-namespace",
     enable: true,
     zenn: {
-      emoji: '📝',
-      slug: '2023-08-18-ts-module-and-namespace',
+      emoji: "📝",
+      slug: "2023-08-18-ts-module-and-namespace",
     },
     qiita: {
-      id: '61a322b8fc7b98208274',
-      url: 'https://qiita.com/hedrall/items/61a322b8fc7b98208274',
-      updatedAt: '2024-01-11T08:04:25+09:00',
+      id: "61a322b8fc7b98208274",
+      url: "https://qiita.com/hedrall/items/61a322b8fc7b98208274",
+      updatedAt: "2024-01-11T08:04:25+09:00",
     },
   },
-  'ts-null-coding': {
-    fileId: 'ts-null-coding',
+  "ts-null-coding": {
+    fileId: "ts-null-coding",
     enable: true,
     zenn: {
-      slug: 'a93fb666400d5a',
-      emoji: '🐕',
+      slug: "a93fb666400d5a",
+      emoji: "🐕",
     },
     qiita: {
-      id: 'e9db422a8accaee661db',
+      id: "e9db422a8accaee661db",
     },
   },
   qwik: {
-    fileId: 'qwik',
+    fileId: "qwik",
     enable: true,
     zenn: {
-      slug: 'c15276e01725e0',
-      emoji: '⚡',
+      slug: "c15276e01725e0",
+      emoji: "⚡",
     },
     qiita: {
-      id: '63795639720bff06d7d1',
+      id: "63795639720bff06d7d1",
     },
   },
-  'doc-creation': {
-    fileId: 'doc-creation',
+  "doc-creation": {
+    fileId: "doc-creation",
     enable: true,
     zenn: {
-      slug: 'doc-creation',
+      slug: "doc-creation",
     },
     qiita: {
-      id: 'e834f4628fc4ff7de1fe',
+      id: "e834f4628fc4ff7de1fe",
     },
   },
-  '20221120-dependency-cruiser': {
-    fileId: '20221120-dependency-cruiser',
+  "20221120-dependency-cruiser": {
+    fileId: "20221120-dependency-cruiser",
     enable: true,
     zenn: {
-      slug: 'f565147bee5da8',
-      emoji: '📑',
+      slug: "f565147bee5da8",
+      emoji: "📑",
     },
     qiita: {
-      id: 'e5507dbec5429c69616f',
+      id: "e5507dbec5429c69616f",
     },
   },
-  '20221120-glob': {
-    fileId: '20221120-glob',
+  "20221120-glob": {
+    fileId: "20221120-glob",
     enable: true,
     zenn: {
-      slug: '81d3812f45c02b',
-      emoji: '✏️',
+      slug: "81d3812f45c02b",
+      emoji: "✏️",
     },
     qiita: {
-      id: '8777ad751dd8ee157d98',
+      id: "8777ad751dd8ee157d98",
     },
   },
-  '20221203-jet-brain': {
+  "20221203-jet-brain": {
     enable: false,
   },
-  'aws-cdk-20210207': {
+  "aws-cdk-20210207": {
     enable: false,
   },
-  'aws-cdk-v2-20210604': {
-    fileId: 'aws-cdk-v2-20210604',
+  "aws-cdk-v2-20210604": {
+    fileId: "aws-cdk-v2-20210604",
     enable: true,
     zenn: {
-      slug: '0c2981db19a952',
-      emoji: '✨',
+      slug: "0c2981db19a952",
+      emoji: "✨",
     },
     qiita: {
-      id: '26f5b021dc3ab01672dd',
+      id: "26f5b021dc3ab01672dd",
     },
   },
-  'cdc-test-20220614': {
-    fileId: 'cdc-test-20220614',
+  "cdc-test-20220614": {
+    fileId: "cdc-test-20220614",
     enable: true,
     zenn: {
-      slug: 'cdc-test-20220614',
-      emoji: '✨',
+      slug: "cdc-test-20220614",
+      emoji: "✨",
     },
     qiita: {
-      id: '91bf077b40dc7cfe05ed',
+      id: "91bf077b40dc7cfe05ed",
     },
   },
-  'esbuild-2021-02-14': {
-    fileId: 'esbuild-2021-02-14',
+  "esbuild-2021-02-14": {
+    fileId: "esbuild-2021-02-14",
     enable: true,
     zenn: {
-      slug: 'esbuild-2021-02-14',
-      emoji: '📦',
+      slug: "esbuild-2021-02-14",
+      emoji: "📦",
     },
     qiita: {
-      id: '2548718cfdf7bef3efc0',
+      id: "2548718cfdf7bef3efc0",
     },
   },
-  'esbuild-dev-env-20211202': {
-    fileId: 'esbuild-dev-env-20211202',
+  "esbuild-dev-env-20211202": {
+    fileId: "esbuild-dev-env-20211202",
     enable: true,
     zenn: {
-      slug: 'esbuild-dev-env-20211202',
-      emoji: '📦',
+      slug: "esbuild-dev-env-20211202",
+      emoji: "📦",
     },
     qiita: {
-      id: '43b6d705ab8dc78e6321',
+      id: "43b6d705ab8dc78e6321",
     },
   },
-  'future-cast-202012': {
+  "future-cast-202012": {
     enable: false,
   },
-  'future-cast-20210213': {
+  "future-cast-20210213": {
     enable: false,
   },
-  'grpc-implementation-20211221': {
-    fileId: 'grpc-implementation-20211221',
+  "grpc-implementation-20211221": {
+    fileId: "grpc-implementation-20211221",
     enable: true,
     zenn: {
-      slug: 'grpc-implementation-20211221',
-      emoji: '📦',
+      slug: "grpc-implementation-20211221",
+      emoji: "📦",
     },
     qiita: {
-      id: '9f4c03c3a6518504473a',
+      id: "9f4c03c3a6518504473a",
     },
   },
-  'grpc-what-20211216': {
-    fileId: 'grpc-what-20211216',
+  "grpc-what-20211216": {
+    fileId: "grpc-what-20211216",
     enable: true,
     zenn: {
-      slug: 'grpc-what-20211216',
-      emoji: '📦',
+      slug: "grpc-what-20211216",
+      emoji: "📦",
     },
     qiita: {
-      id: '038a2b29d27dd631be8a',
+      id: "038a2b29d27dd631be8a",
     },
   },
-  'jp-coding-20230308': {
-    fileId: 'jp-coding-20230308',
+  "jp-coding-20230308": {
+    fileId: "jp-coding-20230308",
     enable: true,
     zenn: {
-      slug: '48dd7d8ae4fab7',
-      emoji: '🧑‍🎓',
+      slug: "48dd7d8ae4fab7",
+      emoji: "🧑‍🎓",
     },
     qiita: {
-      id: 'd90be1e090303de65058',
+      id: "d90be1e090303de65058",
     },
   },
-  'tsoa-20211206': {
-    fileId: 'tsoa-20211206',
+  "tsoa-20211206": {
+    fileId: "tsoa-20211206",
     enable: true,
     zenn: {
-      slug: '09bfa9cd3c765f',
-      emoji: '💨',
+      slug: "09bfa9cd3c765f",
+      emoji: "💨",
     },
     qiita: {
-      id: 'ecc2010d8bc458a7b95e',
+      id: "ecc2010d8bc458a7b95e",
     },
   },
-  'typescript-travel-3.7-4.2-20210516': {
-    fileId: 'typescript-travel-3.7-4.2-20210516',
+  "typescript-travel-3.7-4.2-20210516": {
+    fileId: "typescript-travel-3.7-4.2-20210516",
     enable: true,
     zenn: {
-      slug: '6f94763fc6a1cc',
-      emoji: '🛠️',
+      slug: "6f94763fc6a1cc",
+      emoji: "🛠️",
     },
     qiita: {
-      id: '477417e7b919c00f9a95',
+      id: "477417e7b919c00f9a95",
     },
   },
-  'uvu-jest-20220302': {
-    fileId: 'uvu-jest-20220302',
+  "uvu-jest-20220302": {
+    fileId: "uvu-jest-20220302",
     enable: true,
     zenn: {
-      slug: 'uvu-jest-20220302',
-      emoji: '🧪',
+      slug: "uvu-jest-20220302",
+      emoji: "🧪",
     },
     qiita: {
-      id: 'db1f1de9196fc7d36f3d',
+      id: "db1f1de9196fc7d36f3d",
     },
   },
-  'value-object-20220724': {
-    fileId: 'value-object-20220724',
+  "value-object-20220724": {
+    fileId: "value-object-20220724",
     enable: true,
     zenn: {
-      slug: 'ef51d58507dc71',
-      emoji: '💎',
+      slug: "ef51d58507dc71",
+      emoji: "💎",
     },
     qiita: {
-      id: 'fe9f12f24e79f46edf56',
+      id: "fe9f12f24e79f46edf56",
     },
   },
-  'vite-20210305': {
-    fileId: 'vite-20210305',
+  "vite-20210305": {
+    fileId: "vite-20210305",
     enable: true,
     zenn: {
-      slug: '05ddbca496ba51',
-      emoji: '⚡',
+      slug: "05ddbca496ba51",
+      emoji: "⚡",
     },
     qiita: {
-      id: 'f2239f98f1c0fdd14b07',
-      url: 'https://qiita.com/hedrall/items/f2239f98f1c0fdd14b07',
-      updatedAt: '2023-08-27T20:42:00+09:00',
+      id: "f2239f98f1c0fdd14b07",
+      url: "https://qiita.com/hedrall/items/f2239f98f1c0fdd14b07",
+      updatedAt: "2023-08-27T20:42:00+09:00",
     },
   },
-  'vr-20220510': {
+  "vr-20220510": {
     enable: false,
   },
-  'web-components-20211018': {
-    fileId: 'web-components-20211018',
+  "web-components-20211018": {
+    fileId: "web-components-20211018",
     enable: true,
     zenn: {
-      slug: 'web-components-20211018',
-      emoji: '🧱',
+      slug: "web-components-20211018",
+      emoji: "🧱",
     },
     qiita: {
-      id: 'f2ebb5338f79007083a3',
-      url: 'https://qiita.com/hedrall/items/f2ebb5338f79007083a3',
-      updatedAt: '2023-08-27T20:39:42+09:00',
+      id: "f2ebb5338f79007083a3",
+      url: "https://qiita.com/hedrall/items/f2ebb5338f79007083a3",
+      updatedAt: "2023-08-27T20:39:42+09:00",
     },
   },
 } as const;
