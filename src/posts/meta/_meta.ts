c90560bd1a5e63534e68806a7518751202bfc1f1
hedrall <masaharu73@gmail.com>
2024-01-17 09:00:59 +0900

    fix

--- src/posts/meta/_meta.ts
+++ src/posts/meta/_meta.ts
@@ -2,211 +2,296 @@
 // prettier-ignore
 export default {
   "2024-01-17-not-graphql-on-sc-sys.mdx": {
-    slug: "2024-01-17-not-graphql-on-sc-sys",
-    title: "サーバクライアント型のシステムでGraphQLの採用を避けた方が良いと思う理由",
-    date: "2024-01-17",
-    author: "hedrall",
-    image: "/images/graphql/logo.png",
-    tags: ["graphql", "open-api"],
+    "slug": "2024-01-17-not-graphql-on-sc-sys",
+    "title": "サーバクライアント型のシステムでGraphQLの採用を避けた方が良いと思う理由",
+    "date": "2024-01-17",
+    "author": "hedrall",
+    "image": "/images/graphql/logo.png",
+    "tags": [
+      "graphql",
+      "open-api"
+    ]
   },
   "2023-10-24-no-more-rest.mdx": {
-    slug: "2023-10-24-no-more-rest",
-    title: "GraphQLとOpenAPIを比較する時は「REST API」に注意する",
-    date: "2023-10-24",
-    author: "hedrall",
-    image: "/images/api/icon.png",
-    tags: ["open-api"],
+    "slug": "2023-10-24-no-more-rest",
+    "title": "GraphQLとOpenAPIを比較する時は「REST API」に注意する",
+    "date": "2023-10-24",
+    "author": "hedrall",
+    "image": "/images/api/icon.png",
+    "tags": [
+      "open-api"
+    ]
   },
   "2023-08-18-ts-module-and-namespace.mdx": {
-    slug: "2023-08-18-ts-module-and-namespace",
-    title: "TypeScriptで型と値を一体化し、モジュールを形成する重要性と手法",
-    date: "2023-08-18",
-    author: "hedrall",
-    image: "/images/typescript/logo.png",
-    tags: ["typescript"],
+    "slug": "2023-08-18-ts-module-and-namespace",
+    "title": "TypeScriptで型と値を一体化し、モジュールを形成する重要性と手法",
+    "date": "2023-08-18",
+    "author": "hedrall",
+    "image": "/images/typescript/logo.png",
+    "tags": [
+      "typescript"
+    ]
   },
   "ts-null-coding.mdx": {
-    slug: "ts-null-coding",
-    title: "TypeScriptのコーディングでnullとundefinedを使い分けるべきか？",
-    date: "2023-07-08",
-    author: "hedrall",
-    image: "/images/typescript/logo.png",
-    tags: ["typescript"],
+    "slug": "ts-null-coding",
+    "title": "TypeScriptのコーディングでnullとundefinedを使い分けるべきか？",
+    "date": "2023-07-08",
+    "author": "hedrall",
+    "image": "/images/typescript/logo.png",
+    "tags": [
+      "typescript"
+    ]
   },
   "qwik.mdx": {
-    slug: "qwik",
-    title: "qwikをざっくり調べてみた",
-    date: "2023-06-12",
-    author: "hedrall",
-    image: "https://github.com/hedrall/hedrall-blog/assets/20538481/e724cf4e-0c79-488f-a0c8-003ceac28e0b",
-    tags: ["typescript", "spa"],
+    "slug": "qwik",
+    "title": "qwikをざっくり調べてみた",
+    "date": "2023-06-12",
+    "author": "hedrall",
+    "image": "https://github.com/hedrall/hedrall-blog/assets/20538481/e724cf4e-0c79-488f-a0c8-003ceac28e0b",
+    "tags": [
+      "typescript",
+      "spa"
+    ]
   },
   "doc-creation.mdx": {
-    slug: "doc-creation",
-    title: "エンジニアが考える、情報対象性の高い資料作成に重要なこと",
-    date: "2023-06-25",
-    author: "hedrall",
-    image: "",
-    tags: ["doc"],
+    "slug": "doc-creation",
+    "title": "エンジニアが考える、情報対象性の高い資料作成に重要なこと",
+    "date": "2023-06-25",
+    "author": "hedrall",
+    "image": "",
+    "tags": [
+      "doc"
+    ]
   },
   "20221120-dependency-cruiser.mdx": {
-    slug: "20221120-dependency-cruiser",
-    title: "ESLintだけでは守れない。Dependency cruiserによるアーキテクチャー保護",
-    date: "2022-11-29",
-    author: "hedrall",
-    image: "/images/dependency-cruiser/logo.png",
-    tags: ["typescript"],
+    "slug": "20221120-dependency-cruiser",
+    "title": "ESLintだけでは守れない。Dependency cruiserによるアーキテクチャー保護",
+    "date": "2022-11-29",
+    "author": "hedrall",
+    "image": "/images/dependency-cruiser/logo.png",
+    "tags": [
+      "typescript"
+    ]
   },
   "20221120-glob.mdx": {
-    slug: "20221120-glob",
-    title: "Node.jsでglobパターンを、RegExpに変換するBetterな法方",
-    date: "2022-11-20",
-    author: "hedrall",
-    image: "/images/typescript/logo.png",
-    tags: ["tips", "typescript"],
+    "slug": "20221120-glob",
+    "title": "Node.jsでglobパターンを、RegExpに変換するBetterな法方",
+    "date": "2022-11-20",
+    "author": "hedrall",
+    "image": "/images/typescript/logo.png",
+    "tags": [
+      "tips",
+      "typescript"
+    ]
   },
   "20221203-jet-brain.mdx": {
-    slug: "20221203-jet-brain",
-    title: "[TBD]JetBrainに関するTips集",
-    date: "2023-03-01",
-    author: "hedrall",
-    image: "/images/jetbrain/logo.png",
-    tags: ["typescript"],
+    "slug": "20221203-jet-brain",
+    "title": "[TBD]JetBrainに関するTips集",
+    "date": "2023-03-01",
+    "author": "hedrall",
+    "image": "/images/jetbrain/logo.png",
+    "tags": [
+      "typescript"
+    ]
   },
   "aws-cdk-20210207.mdx": {
-    slug: "aws-cdk-20210207",
-    title: "改めて「AWS CDK」を深掘りしてみた",
-    date: "2021-02-07",
-    author: "hedrall",
-    image: "/images/aws-cdk/logo.jpg",
-    tags: ["infra", "cdk", "typescript"],
+    "slug": "aws-cdk-20210207",
+    "title": "改めて「AWS CDK」を深掘りしてみた",
+    "date": "2021-02-07",
+    "author": "hedrall",
+    "image": "/images/aws-cdk/logo.jpg",
+    "tags": [
+      "infra",
+      "cdk",
+      "typescript"
+    ]
   },
   "aws-cdk-v2-20210604.mdx": {
-    slug: "aws-cdk-v2-20210604",
-    title: "AWS CDK v2 RCが出たので変更点をまとめます！",
-    date: "2021-06-04",
-    author: "hedrall",
-    image: "/images/aws-cdk/logo.jpg",
-    tags: ["infra", "cdk", "typescript"],
+    "slug": "aws-cdk-v2-20210604",
+    "title": "AWS CDK v2 RCが出たので変更点をまとめます！",
+    "date": "2021-06-04",
+    "author": "hedrall",
+    "image": "/images/aws-cdk/logo.jpg",
+    "tags": [
+      "infra",
+      "cdk",
+      "typescript"
+    ]
   },
   "cdc-test-20220614.mdx": {
-    slug: "cdc-test-20220614",
-    title: "マイクロサービス間の整合性を守る、消費者駆動契約テストをNode.jsで試してみる",
-    date: "2022-06-14",
-    author: "hedrall",
-    image: "/images/pact/logo.png",
-    tags: ["typescript", "test"],
+    "slug": "cdc-test-20220614",
+    "title": "マイクロサービス間の整合性を守る、消費者駆動契約テストをNode.jsで試してみる",
+    "date": "2022-06-14",
+    "author": "hedrall",
+    "image": "/images/pact/logo.png",
+    "tags": [
+      "typescript",
+      "test"
+    ]
   },
   "esbuild-2021-02-14.mdx": {
-    slug: "esbuild-2021-02-14",
-    title: "話題のesbuildをさっくりと調査してみた",
-    date: "2021-02-14",
-    author: "hedrall",
-    image: "/images/esbuild/logo.png",
-    tags: ["build", "typescript", "react"],
+    "slug": "esbuild-2021-02-14",
+    "title": "話題のesbuildをさっくりと調査してみた",
+    "date": "2021-02-14",
+    "author": "hedrall",
+    "image": "/images/esbuild/logo.png",
+    "tags": [
+      "build",
+      "typescript",
+      "react"
+    ]
   },
   "esbuild-dev-env-20211202.mdx": {
-    slug: "esbuild-dev-env-20211202",
-    title: "esbuild + React(TS) で実現する超軽量な開発環境",
-    date: "2021-12-02",
-    author: "hedrall",
-    image: "/images/esbuild/logo.png",
-    tags: ["build", "typescript", "react"],
+    "slug": "esbuild-dev-env-20211202",
+    "title": "esbuild + React(TS) で実現する超軽量な開発環境",
+    "date": "2021-12-02",
+    "author": "hedrall",
+    "image": "/images/esbuild/logo.png",
+    "tags": [
+      "build",
+      "typescript",
+      "react"
+    ]
   },
   "future-cast-202012.mdx": {
-    slug: "future-cast-202012",
-    title: "[Nuxt + Firebase + Lambda] で実現したLow CostなSSR環境 (結構辛い)",
-    date: "2020-12-01",
-    author: "hedrall",
-    image: "/images/future-cast/top.png",
-    tags: ["typescript", "firebase", "nuxt"],
+    "slug": "future-cast-202012",
+    "title": "[Nuxt + Firebase + Lambda] で実現したLow CostなSSR環境 (結構辛い)",
+    "date": "2020-12-01",
+    "author": "hedrall",
+    "image": "/images/future-cast/top.png",
+    "tags": [
+      "typescript",
+      "firebase",
+      "nuxt"
+    ]
   },
   "future-cast-20210213.mdx": {
-    slug: "future-cast-20210213",
-    title: "FUTURE CASTという近未来の出来事を予想して遊べるサイトを公開しました！",
-    date: "2021-02-13",
-    author: "hedrall",
-    image: "/images/future-cast/top.jpg",
-    tags: ["typescript", "firebase", "nuxt"],
+    "slug": "future-cast-20210213",
+    "title": "FUTURE CASTという近未来の出来事を予想して遊べるサイトを公開しました！",
+    "date": "2021-02-13",
+    "author": "hedrall",
+    "image": "/images/future-cast/top.jpg",
+    "tags": [
+      "typescript",
+      "firebase",
+      "nuxt"
+    ]
   },
   "grpc-implementation-20211221.mdx": {
-    slug: "grpc-implementation-20211221",
-    title: "Node.js + TypeScript で gRPCに入門する [後編: 実装編]",
-    date: "2021-12-21",
-    author: "hedrall",
-    image: "/images/grpc/logo.jpg",
-    tags: ["api", "typescript", "node.js"],
+    "slug": "grpc-implementation-20211221",
+    "title": "Node.js + TypeScript で gRPCに入門する [後編: 実装編]",
+    "date": "2021-12-21",
+    "author": "hedrall",
+    "image": "/images/grpc/logo.jpg",
+    "tags": [
+      "api",
+      "typescript",
+      "node.js"
+    ]
   },
   "grpc-what-20211216.mdx": {
-    slug: "grpc-what-20211216",
-    title: "Node.js + TypeScript で gRPCに入門する [前編: gRPCとは]",
-    date: "2021-12-16",
-    author: "hedrall",
-    image: "/images/grpc/logo.jpg",
-    tags: ["api", "typescript", "node.js"],
+    "slug": "grpc-what-20211216",
+    "title": "Node.js + TypeScript で gRPCに入門する [前編: gRPCとは]",
+    "date": "2021-12-16",
+    "author": "hedrall",
+    "image": "/images/grpc/logo.jpg",
+    "tags": [
+      "api",
+      "typescript",
+      "node.js"
+    ]
   },
   "jp-coding-20230308.mdx": {
-    slug: "jp-coding-20230308",
-    title: "Webエンジニアはそろそろ日本語変数名の利用を検討してもいいのでは？",
-    date: "2023-03-08",
-    author: "hedrall",
-    image: "/images/typescript/logo.png",
-    tags: ["typescript"],
+    "slug": "jp-coding-20230308",
+    "title": "Webエンジニアはそろそろ日本語変数名の利用を検討してもいいのでは？",
+    "date": "2023-03-08",
+    "author": "hedrall",
+    "image": "/images/typescript/logo.png",
+    "tags": [
+      "typescript"
+    ]
   },
   "tsoa-20211206.mdx": {
-    slug: "tsoa-20211206",
-    title: "Code Firstで疲弊しないOpenApi活用方法",
-    date: "2021-12-10",
-    author: "hedrall",
-    image: "/images/typescript/logo.png",
-    tags: ["api", "open-api", "typescript"],
+    "slug": "tsoa-20211206",
+    "title": "Code Firstで疲弊しないOpenApi活用方法",
+    "date": "2021-12-10",
+    "author": "hedrall",
+    "image": "/images/typescript/logo.png",
+    "tags": [
+      "api",
+      "open-api",
+      "typescript"
+    ]
   },
   "typescript-travel-3.7-4.2-20210516.mdx": {
-    slug: "typescript-travel-3.7-4.2-20210516",
-    title: "TypeScript 3.7 ~ 4.2 までの主な変更をまとめて復習する",
-    date: "2021-05-16",
-    author: "hedrall",
-    image: "/images/typescript/logo.png",
-    tags: ["typescript"],
+    "slug": "typescript-travel-3.7-4.2-20210516",
+    "title": "TypeScript 3.7 ~ 4.2 までの主な変更をまとめて復習する",
+    "date": "2021-05-16",
+    "author": "hedrall",
+    "image": "/images/typescript/logo.png",
+    "tags": [
+      "typescript"
+    ]
   },
   "uvu-jest-20220302.mdx": {
-    slug: "uvu-jest-20220302",
-    title: "既存のJestで書かれたテストを爆速実行するツール、uvu-jest を作りました",
-    date: "2022-03-02",
-    author: "hedrall",
-    image: "/images/aws-cdk/logo.jpg",
-    tags: ["test", "front", "typescript", "api"],
+    "slug": "uvu-jest-20220302",
+    "title": "既存のJestで書かれたテストを爆速実行するツール、uvu-jest を作りました",
+    "date": "2022-03-02",
+    "author": "hedrall",
+    "image": "/images/aws-cdk/logo.jpg",
+    "tags": [
+      "test",
+      "front",
+      "typescript",
+      "api"
+    ]
   },
   "value-object-20220724.mdx": {
-    slug: "value-object-20220724",
-    title: "ドメイン駆動設計: TypeScriptでの値オブジェクトの書き方",
-    date: "2022-07-24",
-    author: "hedrall",
-    image: "/images/typescript/logo-wide.svg",
-    tags: ["typescript", "architecture", "ddd"],
+    "slug": "value-object-20220724",
+    "title": "ドメイン駆動設計: TypeScriptでの値オブジェクトの書き方",
+    "date": "2022-07-24",
+    "author": "hedrall",
+    "image": "/images/typescript/logo-wide.svg",
+    "tags": [
+      "typescript",
+      "architecture",
+      "ddd"
+    ]
   },
   "vite-20210305.mdx": {
-    slug: "vite-20210305",
-    title: "[Vueの開発体験を爆上げする] Viteを調査",
-    date: "2021-03-05",
-    author: "hedrall",
-    image: "/images/aws-cdk/logo.jpg",
-    tags: ["front", "typescript", "vue", "build"],
+    "slug": "vite-20210305",
+    "title": "[Vueの開発体験を爆上げする] Viteを調査",
+    "date": "2021-03-05",
+    "author": "hedrall",
+    "image": "/images/aws-cdk/logo.jpg",
+    "tags": [
+      "front",
+      "typescript",
+      "vue",
+      "build"
+    ]
   },
   "vr-20220510.mdx": {
-    slug: "vr-20220510",
-    title: "Visual Regression テストのやり方を詳しく解説",
-    date: "2022-05-10",
-    author: "hedrall",
-    image: "/images/aws-cdk/logo.jpg",
-    tags: ["typescript", "test", "cdk"],
+    "slug": "vr-20220510",
+    "title": "Visual Regression テストのやり方を詳しく解説",
+    "date": "2022-05-10",
+    "author": "hedrall",
+    "image": "/images/aws-cdk/logo.jpg",
+    "tags": [
+      "typescript",
+      "test",
+      "cdk"
+    ]
   },
   "web-components-20211018.mdx": {
-    slug: "web-components-20211018",
-    title: "WebComponentsとは何だったのか？基礎から再入門する",
-    date: "2021-10-18",
-    author: "hedrall",
-    image: "/images/aws-cdk/logo.jpg",
-    tags: ["front", "web-standard"],
-  },
+    "slug": "web-components-20211018",
+    "title": "WebComponentsとは何だったのか？基礎から再入門する",
+    "date": "2021-10-18",
+    "author": "hedrall",
+    "image": "/images/aws-cdk/logo.jpg",
+    "tags": [
+      "front",
+      "web-standard"
+    ]
+  }
 } as const;
--- src/scripts/投稿ツール/externalSiteSetting/_index.ts
+++ src/scripts/投稿ツール/externalSiteSetting/_index.ts
@@ -2,258 +2,258 @@
 // prettier-ignore
 export default {
   "2024-01-17-not-graphql-on-sc-sys": {
-    fileId: "2024-01-17-not-graphql-on-sc-sys",
-    enable: true,
-    zenn: {
-      emoji: "🤔",
-      slug: "2024-01-17-not-graphql-on-sc-sys",
+    "fileId": "2024-01-17-not-graphql-on-sc-sys",
+    "enable": true,
+    "zenn": {
+      "emoji": "🤔",
+      "slug": "2024-01-17-not-graphql-on-sc-sys"
     },
-    qiita: {
-      id: "b2abaaf44ba6dc305b52",
-      url: "https://qiita.com/hedrall/items/b2abaaf44ba6dc305b52",
-      updatedAt: "2024-01-17T07:56:00+09:00",
-    },
+    "qiita": {
+      "id": "b2abaaf44ba6dc305b52",
+      "url": "https://qiita.com/hedrall/items/b2abaaf44ba6dc305b52",
+      "updatedAt": "2024-01-17T07:56:00+09:00"
+    }
   },
   "2023-10-24-no-more-rest": {
-    fileId: "2023-10-24-no-more-rest",
-    enable: true,
-    zenn: {
-      emoji: "",
-      slug: "2023-10-24-no-more-rest",
+    "fileId": "2023-10-24-no-more-rest",
+    "enable": true,
+    "zenn": {
+      "emoji": "",
+      "slug": "2023-10-24-no-more-rest"
     },
-    qiita: {
-      id: "a1a486993ff000eb6809",
-      url: "https://qiita.com/hedrall/items/a1a486993ff000eb6809",
-      updatedAt: "2024-01-11T08:04:22+09:00",
-    },
+    "qiita": {
+      "id": "a1a486993ff000eb6809",
+      "url": "https://qiita.com/hedrall/items/a1a486993ff000eb6809",
+      "updatedAt": "2024-01-11T08:04:22+09:00"
+    }
   },
   "2023-08-18-ts-module-and-namespace": {
-    fileId: "2023-08-18-ts-module-and-namespace",
-    enable: true,
-    zenn: {
-      emoji: "📝",
-      slug: "2023-08-18-ts-module-and-namespace",
+    "fileId": "2023-08-18-ts-module-and-namespace",
+    "enable": true,
+    "zenn": {
+      "emoji": "📝",
+      "slug": "2023-08-18-ts-module-and-namespace"
     },
-    qiita: {
-      id: "61a322b8fc7b98208274",
-      url: "https://qiita.com/hedrall/items/61a322b8fc7b98208274",
-      updatedAt: "2024-01-11T08:04:25+09:00",
-    },
+    "qiita": {
+      "id": "61a322b8fc7b98208274",
+      "url": "https://qiita.com/hedrall/items/61a322b8fc7b98208274",
+      "updatedAt": "2024-01-11T08:04:25+09:00"
+    }
   },
   "ts-null-coding": {
-    fileId: "ts-null-coding",
-    enable: true,
-    zenn: {
-      slug: "a93fb666400d5a",
-      emoji: "🐕",
+    "fileId": "ts-null-coding",
+    "enable": true,
+    "zenn": {
+      "slug": "a93fb666400d5a",
+      "emoji": "🐕"
     },
-    qiita: {
-      id: "e9db422a8accaee661db",
-    },
+    "qiita": {
+      "id": "e9db422a8accaee661db"
+    }
   },
-  qwik: {
-    fileId: "qwik",
-    enable: true,
-    zenn: {
-      slug: "c15276e01725e0",
-      emoji: "⚡",
+  "qwik": {
+    "fileId": "qwik",
+    "enable": true,
+    "zenn": {
+      "slug": "c15276e01725e0",
+      "emoji": "⚡"
     },
-    qiita: {
-      id: "63795639720bff06d7d1",
-    },
+    "qiita": {
+      "id": "63795639720bff06d7d1"
+    }
   },
   "doc-creation": {
-    fileId: "doc-creation",
-    enable: true,
-    zenn: {
-      slug: "doc-creation",
+    "fileId": "doc-creation",
+    "enable": true,
+    "zenn": {
+      "slug": "doc-creation"
     },
-    qiita: {
-      id: "e834f4628fc4ff7de1fe",
-    },
+    "qiita": {
+      "id": "e834f4628fc4ff7de1fe"
+    }
   },
   "20221120-dependency-cruiser": {
-    fileId: "20221120-dependency-cruiser",
-    enable: true,
-    zenn: {
-      slug: "f565147bee5da8",
-      emoji: "📑",
+    "fileId": "20221120-dependency-cruiser",
+    "enable": true,
+    "zenn": {
+      "slug": "f565147bee5da8",
+      "emoji": "📑"
     },
-    qiita: {
-      id: "e5507dbec5429c69616f",
-    },
+    "qiita": {
+      "id": "e5507dbec5429c69616f"
+    }
   },
   "20221120-glob": {
-    fileId: "20221120-glob",
-    enable: true,
-    zenn: {
-      slug: "81d3812f45c02b",
-      emoji: "✏️",
+    "fileId": "20221120-glob",
+    "enable": true,
+    "zenn": {
+      "slug": "81d3812f45c02b",
+      "emoji": "✏️"
     },
-    qiita: {
-      id: "8777ad751dd8ee157d98",
-    },
+    "qiita": {
+      "id": "8777ad751dd8ee157d98"
+    }
   },
   "20221203-jet-brain": {
-    enable: false,
+    "enable": false
   },
   "aws-cdk-20210207": {
-    enable: false,
+    "enable": false
   },
   "aws-cdk-v2-20210604": {
-    fileId: "aws-cdk-v2-20210604",
-    enable: true,
-    zenn: {
-      slug: "0c2981db19a952",
-      emoji: "✨",
+    "fileId": "aws-cdk-v2-20210604",
+    "enable": true,
+    "zenn": {
+      "slug": "0c2981db19a952",
+      "emoji": "✨"
     },
-    qiita: {
-      id: "26f5b021dc3ab01672dd",
-    },
+    "qiita": {
+      "id": "26f5b021dc3ab01672dd"
+    }
   },
   "cdc-test-20220614": {
-    fileId: "cdc-test-20220614",
-    enable: true,
-    zenn: {
-      slug: "cdc-test-20220614",
-      emoji: "✨",
+    "fileId": "cdc-test-20220614",
+    "enable": true,
+    "zenn": {
+      "slug": "cdc-test-20220614",
+      "emoji": "✨"
     },
-    qiita: {
-      id: "91bf077b40dc7cfe05ed",
-    },
+    "qiita": {
+      "id": "91bf077b40dc7cfe05ed"
+    }
   },
   "esbuild-2021-02-14": {
-    fileId: "esbuild-2021-02-14",
-    enable: true,
-    zenn: {
-      slug: "esbuild-2021-02-14",
-      emoji: "📦",
+    "fileId": "esbuild-2021-02-14",
+    "enable": true,
+    "zenn": {
+      "slug": "esbuild-2021-02-14",
+      "emoji": "📦"
     },
-    qiita: {
-      id: "2548718cfdf7bef3efc0",
-    },
+    "qiita": {
+      "id": "2548718cfdf7bef3efc0"
+    }
   },
   "esbuild-dev-env-20211202": {
-    fileId: "esbuild-dev-env-20211202",
-    enable: true,
-    zenn: {
-      slug: "esbuild-dev-env-20211202",
-      emoji: "📦",
+    "fileId": "esbuild-dev-env-20211202",
+    "enable": true,
+    "zenn": {
+      "slug": "esbuild-dev-env-20211202",
+      "emoji": "📦"
     },
-    qiita: {
-      id: "43b6d705ab8dc78e6321",
-    },
+    "qiita": {
+      "id": "43b6d705ab8dc78e6321"
+    }
   },
   "future-cast-202012": {
-    enable: false,
+    "enable": false
   },
   "future-cast-20210213": {
-    enable: false,
+    "enable": false
   },
   "grpc-implementation-20211221": {
-    fileId: "grpc-implementation-20211221",
-    enable: true,
-    zenn: {
-      slug: "grpc-implementation-20211221",
-      emoji: "📦",
+    "fileId": "grpc-implementation-20211221",
+    "enable": true,
+    "zenn": {
+      "slug": "grpc-implementation-20211221",
+      "emoji": "📦"
     },
-    qiita: {
-      id: "9f4c03c3a6518504473a",
-    },
+    "qiita": {
+      "id": "9f4c03c3a6518504473a"
+    }
   },
   "grpc-what-20211216": {
-    fileId: "grpc-what-20211216",
-    enable: true,
-    zenn: {
-      slug: "grpc-what-20211216",
-      emoji: "📦",
+    "fileId": "grpc-what-20211216",
+    "enable": true,
+    "zenn": {
+      "slug": "grpc-what-20211216",
+      "emoji": "📦"
     },
-    qiita: {
-      id: "038a2b29d27dd631be8a",
-    },
+    "qiita": {
+      "id": "038a2b29d27dd631be8a"
+    }
   },
   "jp-coding-20230308": {
-    fileId: "jp-coding-20230308",
-    enable: true,
-    zenn: {
-      slug: "48dd7d8ae4fab7",
-      emoji: "🧑‍🎓",
+    "fileId": "jp-coding-20230308",
+    "enable": true,
+    "zenn": {
+      "slug": "48dd7d8ae4fab7",
+      "emoji": "🧑‍🎓"
     },
-    qiita: {
-      id: "d90be1e090303de65058",
-    },
+    "qiita": {
+      "id": "d90be1e090303de65058"
+    }
   },
   "tsoa-20211206": {
-    fileId: "tsoa-20211206",
-    enable: true,
-    zenn: {
-      slug: "09bfa9cd3c765f",
-      emoji: "💨",
+    "fileId": "tsoa-20211206",
+    "enable": true,
+    "zenn": {
+      "slug": "09bfa9cd3c765f",
+      "emoji": "💨"
     },
-    qiita: {
-      id: "ecc2010d8bc458a7b95e",
-    },
+    "qiita": {
+      "id": "ecc2010d8bc458a7b95e"
+    }
   },
   "typescript-travel-3.7-4.2-20210516": {
-    fileId: "typescript-travel-3.7-4.2-20210516",
-    enable: true,
-    zenn: {
-      slug: "6f94763fc6a1cc",
-      emoji: "🛠️",
+    "fileId": "typescript-travel-3.7-4.2-20210516",
+    "enable": true,
+    "zenn": {
+      "slug": "6f94763fc6a1cc",
+      "emoji": "🛠️"
     },
-    qiita: {
-      id: "477417e7b919c00f9a95",
-    },
+    "qiita": {
+      "id": "477417e7b919c00f9a95"
+    }
   },
   "uvu-jest-20220302": {
-    fileId: "uvu-jest-20220302",
-    enable: true,
-    zenn: {
-      slug: "uvu-jest-20220302",
-      emoji: "🧪",
+    "fileId": "uvu-jest-20220302",
+    "enable": true,
+    "zenn": {
+      "slug": "uvu-jest-20220302",
+      "emoji": "🧪"
     },
-    qiita: {
-      id: "db1f1de9196fc7d36f3d",
-    },
+    "qiita": {
+      "id": "db1f1de9196fc7d36f3d"
+    }
   },
   "value-object-20220724": {
-    fileId: "value-object-20220724",
-    enable: true,
-    zenn: {
-      slug: "ef51d58507dc71",
-      emoji: "💎",
+    "fileId": "value-object-20220724",
+    "enable": true,
+    "zenn": {
+      "slug": "ef51d58507dc71",
+      "emoji": "💎"
     },
-    qiita: {
-      id: "fe9f12f24e79f46edf56",
-    },
+    "qiita": {
+      "id": "fe9f12f24e79f46edf56"
+    }
   },
   "vite-20210305": {
-    fileId: "vite-20210305",
-    enable: true,
-    zenn: {
-      slug: "05ddbca496ba51",
-      emoji: "⚡",
+    "fileId": "vite-20210305",
+    "enable": true,
+    "zenn": {
+      "slug": "05ddbca496ba51",
+      "emoji": "⚡"
     },
-    qiita: {
-      id: "f2239f98f1c0fdd14b07",
-      url: "https://qiita.com/hedrall/items/f2239f98f1c0fdd14b07",
-      updatedAt: "2023-08-27T20:42:00+09:00",
-    },
+    "qiita": {
+      "id": "f2239f98f1c0fdd14b07",
+      "url": "https://qiita.com/hedrall/items/f2239f98f1c0fdd14b07",
+      "updatedAt": "2023-08-27T20:42:00+09:00"
+    }
   },
   "vr-20220510": {
-    enable: false,
+    "enable": false
   },
   "web-components-20211018": {
-    fileId: "web-components-20211018",
-    enable: true,
-    zenn: {
-      slug: "web-components-20211018",
-      emoji: "🧱",
+    "fileId": "web-components-20211018",
+    "enable": true,
+    "zenn": {
+      "slug": "web-components-20211018",
+      "emoji": "🧱"
     },
-    qiita: {
-      id: "f2ebb5338f79007083a3",
-      url: "https://qiita.com/hedrall/items/f2ebb5338f79007083a3",
-      updatedAt: "2023-08-27T20:39:42+09:00",
-    },
-  },
+    "qiita": {
+      "id": "f2ebb5338f79007083a3",
+      "url": "https://qiita.com/hedrall/items/f2ebb5338f79007083a3",
+      "updatedAt": "2023-08-27T20:39:42+09:00"
+    }
+  }
 } as const;
